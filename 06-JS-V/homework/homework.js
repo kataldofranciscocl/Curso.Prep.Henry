76ce44f1b802721759a800c43c9a72b6b2ed7465
kataldo <kataldo@iMacdekataldo2.home>
2020-09-16 02:23:50 -0300

    06-JS-V

--- 06-JS-V/homework/homework.js
+++ 06-JS-V/homework/homework.js
@@ -9,12 +9,29 @@ function crearUsuario() {
   // Devuelve la clase
   // Tu código:
 
+  function Usuario(opciones){
+    this.usuario = opciones.usuario;
+    this.nombre = opciones.nombre;
+    this.email = opciones.email;
+    this.password = opciones.password;
+  };
+
+  //el método saludar "cuelga" de la clase Usuario
+  Usuario.prototype.saludar = function(){
+    return 'Hola, mi nombre es ' + this.nombre;
+  };
+  
+  return Usuario;
+
 }
 
 function agregarMetodoPrototype(Constructor) {
   // Agrega un método al Constructor del `prototype`
   // El método debe llamarse "saludar" y debe devolver la string "Hello World!"
   // Tu código:
+  Constructor.prototype.saludar = function(){
+    return 'Hello World!';
+  };
 
 }
 
@@ -25,6 +42,19 @@ function agregarStringInvertida() {
   // 'toni'.reverse() => 'inot'
   // Pista: Necesitarás usar "this" dentro de "reverse"
 
+    String.prototype.reverse = function(){
+      //variable donde almacenaremos la string invertida
+      let stringReverse = '';
+      //iteración inversa al objeto
+      //contamos los elementos totales y restamos uno, para tener el numero correcto de elementos
+      //le indicamos que haga la iteración hasta que llegue al elemento en la posición 0
+      //vamos del total restando uno hasta cero
+      for(i = this.length-1; i>=0; i-- ){
+        //asignamos a la variable stringReverse ella misma (vacia) + cada caracter de la iteración.
+        stringReverse = stringReverse + this.charAt(i);
+      }
+      return stringReverse;
+    };
 }
 
 // No modificar nada debajo de esta línea
